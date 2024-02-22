9703af29c368317ef23a6c6d71aacdd061bd6e83
Joaquin Cortes <cort8536@gmail.com>
2024-02-22 15:40:09 -0800

    Update TechnicalAppsDownload.ps1

--- TechnicalAppsDownload.ps1
+++ TechnicalAppsDownload.ps1
@@ -6,6 +6,6 @@ $InstallerPathVbox = "$env:USERPROFILE\downloads\VirtualBox-6.1.50-161033-Win.ex
 Start-Process -FilePath $InstallerPathVbox
 
 #Download and install VScode
-Invoke-WebRequest -Uri https://download.virtualbox.org/virtualbox/6.1.50/VirtualBox-6.1.50-161033-Win.exe -OutFile "$env:USERPROFILE\Downloads\VSCodeUserSetup-x64-1.86.2.exe"
+Invoke-WebRequest -Uri https://code.visualstudio.com/sha/download?build=stable"&"os=win32-x64-user -OutFile "$env:USERPROFILE\Downloads\VSCodeUserSetup-x64-1.86.2.exe"
 $InstallerPathVscode = "$env:USERPROFILE\Downloads\VSCodeUserSetup-x64-1.86.2.exe"
 Start-Process $InstallerPathVscode
