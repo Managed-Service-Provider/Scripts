6b2043daf1f088d520cb15aab72c0c1a8a8be75c
Joaquin Cortes <cort8536@gmail.com>
2024-02-22 13:24:40 -0800

    Add files via upload

---
+++ TechnicalAppsDownload.ps1
@@ -0,0 +1,11 @@
+﻿#Script For Technical Users/Office Manager
+
+#Download and install Virtual Box
+Invoke-WebRequest -Uri https://download.virtualbox.org/virtualbox/7.0.14/VirtualBox-7.0.14-161095-Win.exe -OutFile "$env:USERPROFILE\downloads\VirtualBox-7.0.14-161095-Win.exe"
+$InstallerPathVbox = "$env:USERPROFILE\downloads\VirtualBox-7.0.14-161095-Win.exe"
+Start-Process -FilePath $InstallerPathVbox
+
+#Download and install VScode
+Invoke-WebRequest -Uri https://code.visualstudio.com/sha/download?build=stable"&"os=win32-x64-user -OutFile "$env:USERPROFILE\Downloads\VSCodeUserSetup-x64-1.86.2.exe"
+$InstallerPathVscode = "$env:USERPROFILE\Downloads\VSCodeUserSetup-x64-1.86.2.exe"
+Start-Process $InstallerPathVscode
