d65a9ce3628826cfc41f8ceef23f9dce4dd49515
Joaquin Cortes <cort8536@gmail.com>
2024-02-22 19:19:46 -0500

    Update FileSharing_RDP_Script.ps1

--- FileSharing_RDP_Script.ps1
+++ FileSharing_RDP_Script.ps1
@@ -1,2 +1,10 @@
-﻿#This is to enable file and printer sharingSet-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled true#This is to enable RDP connectionsreg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /fWrite-Output Your Changes have been made sucessfully!
-
+﻿#This is to enable file and printer sharing
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled true
+
+#This is to enable RDP connections
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+Write-Output Your Changes have been made sucessfully!
+
+
+
